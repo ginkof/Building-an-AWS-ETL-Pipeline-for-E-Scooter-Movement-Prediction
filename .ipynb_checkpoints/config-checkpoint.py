3e2c3ffcf2cfd662ed9ef8c823bd84bbaad73f72
ginkof <gian.inkof@gmail.com>
2023-09-20 18:48:01 +0200

    Update al files

---
+++ .ipynb_checkpoints/config-checkpoint.py
@@ -0,0 +1,20 @@
+#This is a file for passwords and credentials
+
+
+
+
+# Credentials to access mySQL
+host = 'localhost'
+schema = 'gans'
+user = 'root'
+password = 'Pirates_92'
+port = 3306
+
+#API key for the whether conditions
+API_key = "0b0ebdc229409b224f2deb5787542447"
+
+
+#API keys for the flights conditions
+content_type = "application/octet-stream"
+X_RapidAPI_Key = "796acd594fmshf05abcd0c047373p161e0ejsn6bbcb86c9b87"
+X_RapidAPI_Host = "aerodatabox.p.rapidapi.com"
